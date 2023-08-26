870aa869c67ccfa77467591bcff47ba19ea67b5c
mar1n <30738216+mar1n@users.noreply.github.com>
2023-08-26 21:08:57 +0100

    extract component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
-const renderBooks = (books) => {
-  return <div data-test="book-list">
-        {
-          books.map((item) => {
-            return <div className="book-item"><h2 className="title">{item.name}</h2></div>
-          })
-        }
-      </div>
-}
+import BookList from './BookList';
 
 function App() {
   const books = [{ name: "Refactoring"}, { name: "Domain-driven design"}]
   return (
     <div className="App">
       <h2 data-test="heading" >Bookish</h2>
-      {
-        renderBooks(books)
-      }
+      <BookList books={books} />
     </div>
   );
 }
