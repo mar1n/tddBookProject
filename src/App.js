cbbd80ba3b09934e051b2b2b0bcbddc8382b03bc
mar1n <30738216+mar1n@users.noreply.github.com>
2023-08-26 20:00:24 +0100

    verify book name

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <h2 data-test="heading" >Bookish</h2>
       <div data-test="book-list">
-        <div className="book-item">Robin Hood</div>
-        <div className="book-item">Robinson Crusoe</div>
+        <div className="book-item"><h2 className="title">Refactoring</h2></div>
+        <div className="book-item"><h2 className="title">Domain-driven design</h2></div>
       </div>
     </div>
   );
