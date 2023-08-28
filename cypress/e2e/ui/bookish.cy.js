a0fbf718e6b23a9e7993a159ec50b3c88c2416b0
mar1n <30738216+mar1n@users.noreply.github.com>
2023-08-28 17:33:32 +0100

    setup and tear down

--- cypress/e2e/ui/bookish.cy.js
+++ cypress/e2e/ui/bookish.cy.js
@@ -1,19 +1,40 @@
 // import cy from "cypress";
 
-describe('Bookish application', () => { 
-    it('Visits the bookish ', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('h2[data-test="heading"').contains('Bookish');
+import axios from "axios";
+
+describe("Bookish application", () => {
+  beforeEach(() => {
+    const books = [
+      { id: 2, name: "Domain-driven design" },
+      { id: 1, name: "Refactoring" },
+    ];
+
+    return books.map((item) =>
+      axios.post("http://localhost:8080/books", item, {
+        headers: { "Content-Type": "application/json" },
+      })
+    );
+  });
+
+  afterEach(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+  it("Visits the bookish ", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('h2[data-test="heading"').contains("Bookish");
+  });
+  it("Shows a book list", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get("div.book-item").should("have.length", 2);
+    cy.get("div.book-item").should((books) => {
+      console.log("books", books);
+      expect(books).to.have.length(2);
+
+      const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
     });
-    it('Shows a book list', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('div[data-test="book-list"]').should('exist');
-        cy.get('div.book-item').should('have.length', 2);
-        cy.get('div.book-item').should((books) => {
-            expect(books).to.have.length(2);
-            
-            const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
-        })
-    });
- })
+  });
+});
