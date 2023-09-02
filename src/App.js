42c5b645a425ab46cd74d6c4e0947feecdc75ad2
mar1n <30738216+mar1n@users.noreply.github.com>
2023-09-02 16:49:39 +0100

    front end routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import BookListContainer from './BookListContainer';
+import { Route, Routes } from "react-router-dom";
+import BookDetailContainer from "./BookDetailContainer";
 
 function App() {
   return (
     <div className="App">
       <h2 data-test="heading" >Bookish</h2>
       <BookListContainer />
+      <Routes>
+        <Route exact path="/" component={BookListContainer} />
+        <Route path='/books/:id' component={BookDetailContainer} />
+      </Routes>
     </div>
   );
 }
