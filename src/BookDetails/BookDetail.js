1dffcdf51d32b9460cd50d0a4729b0badf9f1c4c
mar1n <30738216+mar1n@users.noreply.github.com>
2023-09-03 22:41:40 +0100

    handling default value refactoring

--- src/BookDetails/BookDetail.js
+++ src/BookDetails/BookDetail.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+const getDescriptionFor = (book) => {
+  return book.description ? book.description : book.name;
+};
+
 const BookDetail = ({ book }) => {
-    return (
-        <div className="detail">
-            <h2 className="book-title">{book.name}</h2>
-            <p className="book-description">{book.description}</p>
-        </div>
-    )
-}
+  return (
+    <div className='detail'>
+      <h2 className='book-title'>{book.name}</h2>
+      <p className='book-description' data-testid='book-description'>
+        {getDescriptionFor(book)}
+      </p>
+      {book.description?.length > 10 ? (
+        <a href='http://s' data-testid='show-more'>
+          Show more
+        </a>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 export default BookDetail;
--- src/BookDetails/BookDetail.test.js
+++ src/BookDetails/BookDetail.test.js
@@ -1,29 +1,56 @@
 import { render, screen } from "@testing-library/react";
 import BookDetail from "./BookDetail";
 
-describe('BookDetail', () => {
-    it('renders title', () => {
-        const props = {
-            book: {
-                name: 'Refactoring'
-            }
-        }
-        
-        render(<BookDetail {...props} />);
+describe("BookDetail", () => {
+  it("renders title", () => {
+    const props = {
+      book: {
+        name: "Domain-driven design",
+      },
+    };
 
-        screen.getByText("Refactoring");
-    });
-    it('renders description', () => {
-        const props = {
-            book: {
-                name: 'Refactoring',
-                description: 'Martin Flowers Refactoring defined core ideas and techniques that hundres of thousands of developers have used to improve their software.'
-            }
-        }
+    render(<BookDetail {...props} />);
 
-        render(<BookDetail {...props} />);
+    screen.getAllByText("Domain-driven design");
+  });
+  it("renders description", () => {
+    const props = {
+      book: {
+        name: "Refactoring",
+        description:
+          "Martin Flowers Refactoring defined core ideas and techniques that hundres of thousands of developers have used to improve their software.",
+      },
+    };
 
-        const findThisText = props.book.description;
-        screen.getByText(new RegExp(findThisText, "i"));
-    });
+    render(<BookDetail {...props} />);
+
+    const findThisText = props.book.description;
+    screen.getByText(new RegExp(findThisText, "i"));
+  });
+  it("displays the book name when no description was given", () => {
+    const props = {
+      book: {
+        name: "Refactoring",
+      },
+    };
+
+    render(<BookDetail {...props} />);
+
+    const description = screen.getByTestId("book-description");
+    expect(description.innerHTML).toEqual(props.book.name);
+  });
+  it("Shows *more* link when description is too long", () => {
+    const props = {
+      book: {
+        name: "Refactoring",
+        description: "The book about how to do refactoring ...",
+      },
+    };
+
+    render(<BookDetail {...props} />);
+
+    const link = screen.getByTestId("show-more");
+    const title = screen.getByTestId("book-description");
+    expect(link.innerHTML).toEqual("Show more");
+  });
 });
