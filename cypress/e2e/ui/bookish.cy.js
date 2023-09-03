8ba5fde3f21a3852591ed512874abd2b43ac64a4
mar1n <30738216+mar1n@users.noreply.github.com>
2023-09-03 16:38:52 +0100

    Testting data

--- cypress/e2e/ui/bookish.cy.js
+++ cypress/e2e/ui/bookish.cy.js
@@ -1,27 +1,6 @@
 // import cy from "cypress";
 
-import axios from "axios";
-
 describe("Bookish application", () => {
-  beforeEach(() => {
-    const books = [
-      { id: 3, name: "Building Micro-service" },
-      { id: 2, name: "Domain-driven design" },
-      { id: 1, name: "Refactoring" },
-    ];
-
-    return books.map((item) =>
-      axios.post("http://localhost:8080/books", item, {
-        headers: { "Content-Type": "application/json" },
-      })
-    );
-  });
-
-  afterEach(() => {
-    return axios
-      .delete("http://localhost:8080/books?_cleanup=true")
-      .catch((err) => err);
-  });
   it("Visits the bookish ", () => {
     cy.visit("http://localhost:3000/");
     cy.get('h2[data-test="heading"').contains("Bookish");
@@ -29,12 +8,12 @@ describe("Bookish application", () => {
   it("Shows a book list", () => {
     cy.visit("http://localhost:3000/");
     cy.get('div[data-test="book-list"]').should("exist");
-    cy.get("div.book-item").should("have.length", 3);
+    cy.get("div.book-item").should("have.length", 4);
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(3);
+      expect(books).to.have.length(4);
 
       const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
-      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design", "Building Micro-service"]);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design", "Building Micro-services", "Acceptance Test Driven Development with React"]);
     });
   });
   it('Goes to the detail page', () => {
