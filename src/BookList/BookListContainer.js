f73bad92da037d6ab1935e6a12e258065f7bbf01
mar1n <30738216+mar1n@users.noreply.github.com>
2023-09-10 13:02:07 +0100

    acceptance test

--- src/BookList/BookListContainer.js
+++ src/BookList/BookListContainer.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BookList from "./BookList";
 import { useRemoteService } from "../hooks";
+import { TextField } from "@mui/material";
 
 const BookListContainer = () => {
-    const { data, loading, error } = useRemoteService(`http://localhost:8080/books`,[]);
-    
-    if(loading) {
-        return <p>LOADING...</p>
-    }
-    if(error) {
-        return <p>ERROR...</p>
-    }
-
-    return <BookList books={data} loading={loading} error={error} />
-}
-
+    const [term, setTerm] = useState('');
+    const {data, loading, error, setUrl} = useRemoteService('http://localhost:8080/books', []);
+  
+    useEffect(() => {
+      setUrl(`http://localhost:8080/books?q=${term}`)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [term]);
+  
+    return (<>
+      <TextField
+        label="Search"
+        value={term}
+        data-test="search"
+        onChange={(e) => setTerm(e.target.value)}
+        margin="normal"
+        variant="outlined"
+      />
+      <BookList books={data} loading={loading} error={error}/>
+    </>);
+  }
 export default BookListContainer;
--- src/hooks.js
+++ src/hooks.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useEffect, useState} from "react";
 
-export const useRemoteService = (url, initial) => {
-    const [data, setData] = useState(initial);
+export const useRemoteService = (initialUrl, initialData) => {
+    const [data, setData] = useState(initialData);
+    const [url, setUrl] = useState(initialUrl)
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
@@ -21,7 +22,7 @@ export const useRemoteService = (url, initial) => {
               }
         }
         fetchBooks();
-    }, [])
+    }, [url])
 
-    return { data, loading, error }
+    return { data, loading, error, setUrl }
 }
