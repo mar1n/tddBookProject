28a4e59f8917d72cfb22ab08a1b4a185a024a95c
mar1n <30738216+mar1n@users.noreply.github.com>
2023-11-12 00:33:26 +0000

    async actions

--- src/redux/actions/action.test.js
+++ src/redux/actions/action.test.js
@@ -1,13 +1,51 @@
-import { setSearchTerm } from "./actions";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { setSearchTerm, fetchBooks } from "./actions";
+import axios from "axios";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe("BookListContainer related actions", () => {
-    test('Sets the search keyword', () => {
-        const term = "";
-        const expected = {
-            type: 'SET_SEARCH_TERM',
-            term
-        }
-        const action = setSearchTerm(term);
-        expect(action).toEqual(expected);
-    });
+  test("Sets the search keyword", () => {
+    const term = "";
+    const expected = {
+      type: "SET_SEARCH_TERM",
+      term,
+    };
+    const action = setSearchTerm(term);
+    expect(action).toEqual(expected);
+  });
+  test("Fetches data successfully", () => {
+    const books = [
+      { id: 1, name: "Refactoring" },
+      { id: 2, name: "Domain-driven design" },
+    ];
+
+    axios.get = jest.fn().mockImplementation(() => Promise.resolve({ data: books}));
+
+    const expectedActions = [
+        { type: 'FETCH_BOOKS_PENDING'},
+        { type: 'FETCH_BOOKS_SUCCESS', books}
+    ];
+
+    const store = mockStore({ books: [] });
+
+    return store.dispatch(fetchBooks('')).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+    })
+  });
+  test('Fetch data with error ', () => {
+    axios.get = jest.fn().mockImplementation(() => Promise.reject({ message: 'Something went wrong'}))
+
+    const expectedActions = [
+        { type: 'FETCH_BOOKS_PENDING'},
+        { type: 'FETCH_BOOKS_FAILED', err: 'Something went wrong'}
+    ]
+    const store = mockStore({ books: [] });
+
+    return store.dispatch(fetchBooks('')).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+    })
+  });
 });
--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,3 +1,19 @@
+import axios from "axios";
+
+export const fetchBooks = () => {
+  return (dispatch) => {
+    dispatch({ type: "FETCH_BOOKS_PENDING" });
+    return axios
+      .get(`http://localhost:8080/books`)
+      .then((res) => {
+        dispatch({ type: "FETCH_BOOKS_SUCCESS", books: res.data });
+      })
+      .catch((err) => {
+        dispatch({ type: "FETCH_BOOKS_FAILED", err: err.message });
+      });
+  };
+};
+
 export const setSearchTerm = (term) => {
-    return { type: 'SET_SEARCH_TERM', term }
-}
+  return { type: "SET_SEARCH_TERM", term };
+};
